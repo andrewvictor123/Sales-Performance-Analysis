2b5960b4c5a96c4096b9e9d2293bc2402e3b2181
Andrew Victor <avlinkedinacc234@gmail.com>
2026-06-26 05:17:54 +0300

    perform data analysis and export cleaned datasets

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -26,3 +26,72 @@ ALTER TABLE superstore_dataset
 DROP COLUMN Order_Date;
 
 EXEC sp_rename 'superstore_dataset.Order_Date_Fixed', 'Order_Date', 'COLUMN';
+
+
+
+
+
+SELECT 
+    ROUND(SUM(Sales), 2) AS Total_Sales,
+    ROUND(SUM(Profit), 2) AS Total_Profit,
+    COUNT(DISTINCT Order_ID) AS Total_Orders
+FROM superstore_dataset;
+
+SELECT 
+    YEAR(Order_Date) AS Order_Year,
+    ROUND(SUM(Sales), 2) AS Yearly_Sales
+FROM superstore_dataset
+GROUP BY YEAR(Order_Date)
+ORDER BY Order_Year;
+
+SELECT TOP 5 
+    Category, 
+    ROUND(SUM(Profit), 2) AS Total_Profit
+FROM superstore_dataset
+GROUP BY Category
+ORDER BY Total_Profit DESC;
+
+SELECT TOP 5
+    Country,
+    ROUND(SUM(Sales), 2) AS Total_Sales
+FROM superstore_dataset
+GROUP BY Country
+ORDER BY Total_Sales DESC
+
+SELECT 
+    ROUND(Discount, 2) AS Discount, 
+    ROUND(AVG(Profit), 2) AS Average_Profit
+FROM superstore_dataset
+GROUP BY Discount
+ORDER BY Discount;
+
+
+
+
+
+SELECT 
+    [Row_ID],
+    [Order_ID],
+    TRY_CAST([Order_Date] AS DATE) AS [Order_Date], 
+    TRY_CAST([Ship_Date] AS DATE) AS [Ship_Date],
+    [Ship_Mode],
+    [Customer_ID],
+    [Customer_Name],
+    [Segment],
+    [City],
+    [State],
+    [Country],
+    [Postal_Code],
+    [Market],
+    [Region],
+    [Product_ID],
+    [Category],
+    [Sub_Category],
+    [Product_Name],
+    [Sales],
+    [Quantity],
+    [Discount],
+    [Profit],
+    [Shipping_Cost],
+    [Order_Priority]
+FROM superstore_dataset;
